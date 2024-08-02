3cd50f6389e773f75ab93de914f4fd0f9580e233
Emma Gale <emmajgale@hotmail.com>
2024-08-02 13:29:14 +0100

    add useRef to dropdown menu to close when clicked outside

--- src/components/DropdownNav.jsx
+++ src/components/DropdownNav.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './navbar.module.css';
 
@@ -11,8 +11,23 @@ const DropdownNav = () => {
 		{ route: 'bootcamp-plus-plus', title: 'Bootcamp ++' }
 	];
 
+	const ref = useRef();
+
+	useEffect(() => {
+		const handler = (event) => {
+			console.log('the event ref is', ref);
+			if (isOpen && ref.current && !ref.current.contains(event.target)) {
+				setIsOpen(false);
+			}
+		};
+		document.addEventListener('mousedown', handler);
+		return () => {
+			document.removeEventListener('mousedown', handler);
+		};
+	}, [isOpen]);
+
 	return (
-		<div className={styles.dropdownContainer}>
+		<div className={styles.dropdownContainer} ref={ref}>
 			<button
 				className={styles.dropdownToggle}
 				onClick={() => setIsOpen(!isOpen)}
--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,71 +1,85 @@
-import React, {useState, useRef, useEffect} from 'react';
-import {NavLink} from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './hamburgerMenu.module.css';
 
 function HamburgerMenu() {
-  const [navbarOpen, setNavbarOpen] = useState(false);
+	const [navbarOpen, setNavbarOpen] = useState(false);
 
-  const toggleNavbar = () => setNavbarOpen(!navbarOpen);
-  const closeNavbar = () => setNavbarOpen(false);
+	const toggleNavbar = () => setNavbarOpen(!navbarOpen);
+	const closeNavbar = () => setNavbarOpen(false);
 
-  const ref = useRef();
+	const ref = useRef();
 
-  useEffect(() => {
-    const handler = (event) => {
-      if (navbarOpen && ref.current && !ref.current.contains(event.target)) {
-        setNavbarOpen(false);
-      }
-    };
-    document.addEventListener('mousedown', handler);
-    return () => {
-      document.removeEventListener('mousedown', handler);
-    };
-  }, [navbarOpen]);
+	useEffect(() => {
+		const handler = (event) => {
+			console.log('the event ref is', ref);
+			if (
+				navbarOpen &&
+				ref.current &&
+				!ref.current.contains(event.target)
+			) {
+				setNavbarOpen(false);
+			}
+		};
+		document.addEventListener('mousedown', handler);
+		return () => {
+			document.removeEventListener('mousedown', handler);
+		};
+	}, [navbarOpen]);
 
-  return (
-    <div
-      className={styles.hamburgerMenu}
-      ref={ref}>
-      {/* Hamburger Button */}
-      <button
-        className={styles.hamburgerButton}
-        onClick={toggleNavbar}>
-        <div
-          className={
-            navbarOpen ? styles.hamburgerTopOpen : styles.hamburgerTopClosed
-          }></div>
-        <div
-          className={
-            navbarOpen ? styles.hamburgerMidOpen : styles.hamburgerMidClosed
-          }></div>
-        <div
-          className={
-            navbarOpen
-              ? styles.hamburgerBottomOpen
-              : styles.hamburgerBottomClosed
-          }></div>
-      </button>
+	return (
+		<div className={styles.hamburgerMenu} ref={ref}>
+			{/* Hamburger Button */}
+			<button className={styles.hamburgerButton} onClick={toggleNavbar}>
+				<div
+					className={
+						navbarOpen
+							? styles.hamburgerTopOpen
+							: styles.hamburgerTopClosed
+					}
+				></div>
+				<div
+					className={
+						navbarOpen
+							? styles.hamburgerMidOpen
+							: styles.hamburgerMidClosed
+					}
+				></div>
+				<div
+					className={
+						navbarOpen
+							? styles.hamburgerBottomOpen
+							: styles.hamburgerBottomClosed
+					}
+				></div>
+			</button>
 
-      <div className={navbarOpen ? styles.menu : styles.menuClosed}>
-        {/* Menu */}
-        <ul className={`${styles.smallScreenMenu} ${styles.open}`}>
-          <li className={styles.hamburgerLink}>
-            <NavLink
-              to='/'
-              className={({isActive}) => (isActive ? styles.active : '')}
-              onClick={closeNavbar}>
-              Home
-            </NavLink>{' '}
-          </li>
-          <li className={styles.hamburgerLink}>
-            <NavLink
-              to='/about'
-              className={({isActive}) => (isActive ? styles.active : '')}
-              onClick={closeNavbar}>
-              About
-            </NavLink>
-          </li>
-          {/* <li className={styles.hamburgerLink}>
+			<div className={navbarOpen ? styles.menu : styles.menuClosed}>
+				{/* Menu */}
+				<ul className={`${styles.smallScreenMenu} ${styles.open}`}>
+					<li className={styles.hamburgerLink}>
+						<NavLink
+							to="/"
+							className={({ isActive }) =>
+								isActive ? styles.active : ''
+							}
+							onClick={closeNavbar}
+						>
+							Home
+						</NavLink>{' '}
+					</li>
+					<li className={styles.hamburgerLink}>
+						<NavLink
+							to="/about"
+							className={({ isActive }) =>
+								isActive ? styles.active : ''
+							}
+							onClick={closeNavbar}
+						>
+							About
+						</NavLink>
+					</li>
+					{/* <li className={styles.hamburgerLink}>
             <NavLink
               to="/donate"
               className={({ isActive }) => (isActive ? styles.active : "")}
@@ -74,18 +88,21 @@ function HamburgerMenu() {
               Donate
             </NavLink>
           </li> */}
-          <li className={styles.hamburgerLink}>
-            <NavLink
-              to='/contact'
-              className={({isActive}) => (isActive ? styles.active : '')}
-              onClick={closeNavbar}>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+					<li className={styles.hamburgerLink}>
+						<NavLink
+							to="/contact"
+							className={({ isActive }) =>
+								isActive ? styles.active : ''
+							}
+							onClick={closeNavbar}
+						>
+							Contact
+						</NavLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default HamburgerMenu;

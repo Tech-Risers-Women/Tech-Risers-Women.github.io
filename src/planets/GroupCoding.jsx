d8c41c9c24b064a68b2cb26fbe530be71fc198e3
gros-pataplouf <sceres@pm.me>
2024-08-04 20:15:21 +0200

    add group coding sessions

--- src/planets/GroupCoding.jsx
+++ src/planets/GroupCoding.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Banner from '../moons/Banner'
 import styles from './groupcoding.module.css'
+import eniacImage from '../media/ENIAC.webp'
+
 
 function GroupCoding() {
   return (
     <div className={styles.groupcoding}>
     <Banner name={"Group Coding Sessions"}/>
-    <div>Lorem ipsum</div>
+    <div className={styles.groupcodingTile}>
+
+        <div>
+          <p>
+          Our Group Coding sessions are held every Wednesday evening. They focus on hands-on skill development through pair programming and Test Driven Development - a safe space to practice and discuss code with other women. <br />
+          We work on coding katas in Java, JavaScript and Python. The program runs in cycles, starting multiple times per year.       
+          </p>
+
+          <Link
+           to="https://docs.google.com/forms/d/1avQMSWMHpTmXv3atDX1hk3ZUU2T7xuw3M30nuEIXGpU/viewform?edit_requested=true" target="_blank">
+            Join us
+
+          </Link>
+        </div>
+        <div >
+          <img
+            className={styles.groupcodingImage}
+            src={eniacImage}
+            alt="The Women employed by the ENIAC project"
+          />
+        </div>
+
+    </div>
     </div>
 
   )

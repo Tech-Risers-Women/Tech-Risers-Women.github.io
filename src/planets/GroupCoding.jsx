e608bb0e4cace0d14cd9105c436feb16288a3604
gros-pataplouf <sceres@pm.me>
2024-08-05 19:47:05 +0200

    amend group coding content

--- src/planets/GroupCoding.jsx
+++ src/planets/GroupCoding.jsx
@@ -1,42 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Banner from '../moons/Banner'
-import styles from './groupcoding.module.css'
-import { LuHeartHandshake } from "react-icons/lu"
-import eniacImage from '../media/ENIAC.webp'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Banner from "../moons/Banner";
+import styles from "./groupcoding.module.css";
+import { LuHeartHandshake } from "react-icons/lu";
+import eniacImage from "../media/ENIAC.webp";
 
 function GroupCoding() {
   return (
     <div className={styles.groupcoding}>
-    <Banner name={"Group Coding Sessions"}/>
-    <div className={styles.groupcodingTile}>
-
+      <Banner name={"Group Coding Sessions"} />
+      <div className={styles.groupcodingTile}>
         <div className={styles.groupcodingText}>
           <p>
-          Our Group Coding sessions are held on Wednesday evenings. They focus on hands-on skill development through pair programming and Test Driven Development - a safe space to practice and discuss code with other women. <br />
-          We work on coding katas in Java, JavaScript and Python. The program runs in cycles, starting multiple times per year.       
+            Our group coding session are a continuous practice group focused on
+            hands-on skill development. We apply Test Driven Design/Development
+            and good coding practices while Ensemble coding on Katas and
+            practice applications. We onboard new starters a few times a year.
+            Sign-up here to get notified about the next session.
           </p>
-          <Link className={styles.groupcodingJoin}
-           to="https://docs.google.com/forms/d/1avQMSWMHpTmXv3atDX1hk3ZUU2T7xuw3M30nuEIXGpU/viewform?edit_requested=true" target="_blank">
-
-          <LuHeartHandshake />
-          <p className={styles.groupcodingCall}>Join us</p>
-
+          <Link
+            className={styles.groupcodingJoin}
+            to="https://docs.google.com/forms/d/1avQMSWMHpTmXv3atDX1hk3ZUU2T7xuw3M30nuEIXGpU/viewform?edit_requested=true"
+            target="_blank"
+          >
+            <LuHeartHandshake />
+            <p className={styles.groupcodingCall}>Sign up</p>
           </Link>
         </div>
-        <div >
+        <div>
           <img
             className={styles.groupcodingImage}
             src={eniacImage}
             alt="The Women employed by the ENIAC project"
           />
         </div>
+      </div>
 
     </div>
-    </div>
-
-  )
+  );
 }
 
-export default GroupCoding
+export default GroupCoding;

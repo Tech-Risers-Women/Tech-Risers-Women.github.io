a5219cd198ae190bbe643c1c1b7345b66b4c6e25
gros-pataplouf <sceres@pm.me>
2024-08-14 22:26:12 +0200

    feat: add first draft of Architect.jsx, photo needs styling

--- src/planets/Architect.jsx
+++ src/planets/Architect.jsx
@@ -1,59 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Banner from '../moons/Banner'
-import Stack from '../moons/Stack'
-import { LuHeartHandshake } from "react-icons/lu";
-import hamiltonImage from '../media/MargeretHamilton.avif';
-import styles from './architect.module.css'
-
+import Banner from "../moons/Banner";
+import Initiative from "./Initiative";
+import InitiativePhoto from "../moons/InitiativePhoto";
+import hamiltonImage from "../media/MargeretHamilton.avif";
 
 function Architect() {
+  const initiative = {
+    initiativeName: "The Architects' Kata Club",
+    initiativeText: `The Architects Kata Club is where we practice designing systems for various domains and technical problems. What better way to improve your architecture skills than to practice them and gain exposure, beyond what you get to do at work. Share and grow your knowledge of architectural patterns and design principles. In small groups, participants tackle different katas in a collaborative environment, moderated to ensure smooth progress and skill application.`,
+    initiativeLink:
+      "https://docs.google.com/forms/d/1avQMSWMHpTmXv3atDX1hk3ZUU2T7xuw3M30nuEIXGpU/viewform?edit_requested=true",
+    initiativeImage: hamiltonImage,
+    initiativeImageAlt:
+      "Margeret Hamilton standing next to the printout of her Apollo program.",
+    initiativeFAQs:[ {
+      summary: "What do I need to know before attending?",
+      details:
+        "Participants should have experience in deploying features to production systems.",
+    }]
+  };
   return (
-    <div className={styles.architect}>
-    <div className={styles.architect}>
-    <Banner name={"The Architects' Kata Club"}/>
-      <div className={styles.architectTile}>
-        <div className={styles.architectText}>
-          <p>
-          The Architects Kata Club is where we practice designing systems for various domains and technical problems. What better way to improve your architecture skills than to practice them and gain exposure, beyond what you get to do at work. Share and grow your knowledge of architectural patterns and design principles. In small groups, participants tackle different katas in a collaborative environment, moderated to ensure smooth progress and skill application.
-          </p>
-          <Link
-            className={styles.architectJoin}
-            to="https://docs.google.com/forms/d/1avQMSWMHpTmXv3atDX1hk3ZUU2T7xuw3M30nuEIXGpU/viewform?edit_requested=true"
-            target="_blank"
-          >
-            <LuHeartHandshake />
-            <p className={styles.architectCall}>Sign up</p>
-          </Link>
-        </div>
-        <Stack />
-
-        {/* <div>
-        <img
-            className={styles.architectImage}
-            src={hamiltonImage}
-            alt="Margeret Hamilton piling up the printed code of her Apollo program."
-          />
-
-
-        </div> */}
-      </div>
-
-      <div className={`${styles.architectTile} ${styles.architectUnflex}`}>
-
-      <details className={styles.architectQuestions}>
-        <summary>What do I need to know before attending?</summary>
-        <span>
-        Participants should have experience in deploying features to production systems.
-        </span>
-      </details>
-
-      </div>
-    </div>
-
-    </div>
-
-  )
+    <Initiative
+      initiative={{
+        initiativeFAQs: initiative.initiativeFAQs,
+        initiativeLink: initiative.initiativeLink,
+        initiativeText: initiative.initiativeText,
+      }}
+    >
+      <Banner name={initiative.initiativeName} />
+      <InitiativePhoto
+        src={initiative.initiativeImage}
+        alt={initiative.initiativeImageAlt}
+      />
+    </Initiative>
+  );
 }
 
-export default Architect
+export default Architect;

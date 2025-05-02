d6d7454610a729d9effe1c1d099525b413b071c1
Emma Gale <emjogale@users.noreply.github.com>
2025-05-02 15:59:17 +0100

    add details to volunteer with us

--- src/planets/WorkWithUs.jsx
+++ src/planets/WorkWithUs.jsx
@@ -1,20 +1,38 @@
-import React from "react";
-import styles from "./workwithus.module.css";
-import Banner from "../moons/Banner";
-
+import React from 'react';
+import styles from './workwithus.module.css';
+import Banner from '../moons/Banner';
 
 function WorkWithUs() {
-
-  return (
-    <div className={styles.workwithus}>
-      <Banner name={"Work With Us"}/>
-        <div className={styles.workwithusText}>
-                <p>
-                  What to put here...
-                </p>
-        </div>
-    </div>
-  );
+	return (
+		<div className={styles.workwithus}>
+			<Banner name={'Work With Us'} />
+			<div className={styles.workwithusText}>
+				<h2>Volunteer with us!</h2>
+				<p>
+					Tech Risers Women is managed and run by volunteers. We're
+					always on the lookout for people to join us to help with
+					keeping it running. This is an opportunity to share you
+					skills and learn new ones.
+				</p>
+				<ul>
+					<li>Help lead sessions</li>
+					<li>Help with social media</li>
+					<li>Help with general orgainsing</li>
+					<li>
+						Help with any other way you would like to contribute
+					</li>
+				</ul>
+				<a href="https://forms.gle/mT6ySwpGRWDBrjHK7">
+					<button className={styles.getUpdatesButton}>
+						Volunteer with us
+					</button>
+				</a>
+			</div>
+			<div className={styles.workwithusText}>
+				<h2>Partner with us!</h2>
+			</div>
+		</div>
+	);
 }
 
 export default WorkWithUs;

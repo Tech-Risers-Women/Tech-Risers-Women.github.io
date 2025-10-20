237c62fab7e5843a7d61c0c818be9ebaaac2832f
Sarah Usher <sarahnusher@gmail.com>
2025-10-20 13:31:28 +0100

    copy updated

--- src/planets/Bootcamp.jsx
+++ src/planets/Bootcamp.jsx
@@ -8,7 +8,7 @@ function Bootcamp() {
 	const initiative = {
 		initiativeName: 'Bootcamp ++',
 		initiativeText:
-			"Have you completed a bootcamp and started working, but feel you've barely scratched the surface? Are you looking for a friendly place to structure your learning journey and build knowledge in a community-driven environment? Then bootcamp++ might be what you are looking for. Together, we deep-dive into various computer science and software engineering topics which are relevant to our careers. We'll complete practical exercises with peer-reviews, along with occasional workshops from experienced engineers. This is not a course, but rather a pathway you can walk with others, while we rise in our careers together. Please note this initiative is still in the planning stages and is due to be launched in 2025.",
+			"Have you completed a bootcamp and started working, but feel you've barely scratched the surface? Are you looking for a friendly place to structure your learning journey and build knowledge in a community-driven environment? Then bootcamp++ might be what you are looking for. Together, we deep-dive into various computer science and software engineering topics which are relevant to our careers. We'll complete practical exercises with peer-reviews, along with occasional workshops from experienced engineers. This is not a course, but rather a pathway you can walk with others, while we rise in our careers together.",
 		initiativeLink:
 			'https://docs.google.com/forms/d/1avQMSWMHpTmXv3atDX1hk3ZUU2T7xuw3M30nuEIXGpU/viewform?edit_requested=true',
 		initiativeImage: moutonImage,
@@ -17,7 +17,10 @@ function Bootcamp() {
 		initiativeFAQs: [
 			{
 				summary: 'What do I need to know before attending?',
-				details: 'tbc'
+				details: `You should have completed a Bootcamp, and are looking for work, or are now working in tech.
+Sessions are not suitable for beginner coders - please sign-up for Group Coding Sessions if you are new to coding or completing your Bootcamp.
+Self-taught engineers are welcome, but you should know: 1 programming language, and have completed at least 3 portfolio items.
+Sessions are not intended to replace courses. If you would prefer taking a course, please email us and we can send you suggestions based on what you want to cover.`
 			}
 		]
 	};
